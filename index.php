090be34b336f5f10c60ee3af4331b2c229cfa045
Muhammad Zian Ilahee <iamthemzi@iamthemzi.com>
2016-10-26 18:03:59 -0700

    Upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.js"
+  integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+  crossorigin="anonymous"></script>
+<script src="js/recorder.js"></script>
+<script src="js/index.js"></script>
+</head>
+
+<body>
+
+<audio controls autoplay></audio>
+<video controls autoplay></video>
+<input onclick="audio_stream_and_record()" type="button" value="Audio Stream" />
+<input onclick="video_stream_and_record()" type="button" value="Video Stream" />
+
+<div id = "aud_plr">
+  <div id = "aud_plr_inr">
+    <audio controls id="aud_ctl">
+    <source id ="aud_src" src="music/horse.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+    </audio>
+  <br>
+  </div>
+</div>
+
+<?php
+  echo "<br>";
+  if ($handle = opendir('./music')) {
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+	if($entry !== "." && $entry !== ".."){
+           echo "<div onclick='song_play()' class='itm'>$entry</div><br>".PHP_EOL;
+	}
+    }
+
+    closedir($handle);
+}
+?>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,50 @@
+var stream,recorder;
+var song_play = function(){
+   $(".itm").click(function(){
+	$("#aud_src").attr("src", "music/".concat($(this).text()));
+	$("#aud_ctl")[0].pause();
+	$("#aud_ctl")[0].load();
+	$("#aud_ctl")[0].oncanplaythrough = $("#aud_ctl")[0].play();
+   });
+
+}
+
+//Live Audio and Video Stuff
+
+//Error and Success Callbacks
+var onFail = function(e) {
+  console.log('Rejected!', e);
+};
+
+var onSuccess = function(s) {
+  stream = s;
+  var ctx = new AudioContext();
+  var streamSource = ctx.createMediaStreamSource(s);
+  recorder = new Recorder(mediaStreamSource);
+  recorder.record()
+}
+
+
+
+function startRecording() {
+  if (navigator.getMedia) {
+    navigator.getMedia({audio: true,video:true}, onSuccess, onFail);
+  } else {
+    console.log('navigator.getUserMedia not present');
+  }
+}
+
+function audio_stream_and_record() {
+$('audio').attr("src",window.URL.createObjectURL(stream));
+}
+
+function video_stream_and_record() {
+$('video').attr("src",window.URL.createObjectURL(stream));
+}
+
+$(document).ready(function() {
+    window.URL = window.URL || window.webkitURL;
+    navigator.getMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    startRecording();
+});
+
